b523ba89ee7e9b59d1e15e884b0d847ffbbb2a6b
nishankRadicleInc <nsinghal@radicleinc.com>
2021-03-09 14:52:43 +0530

    first commit

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudeexample/Application.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.springboot2jpacrudeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2JpaCrudeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/guides/springboot2/springboot2jpacrudeexample/Springboot2JpaCrudeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.springboot2jpacrudeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
